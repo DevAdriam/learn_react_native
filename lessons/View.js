1eab33ea2d2215729572fa26d11f5b9b20014d27
naingaungzaw <naingaung9863@gmail.com>
2023-11-10 22:57:29 +0630

    add : view tag

---
+++ lessons/View.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View } from "react-native";
+
+const ViewTag = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: "plum" }}>
+            <View
+                style={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: "lightblue",
+                }}
+            ></View>
+            <View
+                style={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: "lightgreen",
+                }}
+            ></View>
+        </View>
+    );
+};
+
+export default ViewTag;
